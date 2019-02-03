0ff32a1d47f907bf7085abbfc0b9f2e93f65f003
ajimoukhuede <44909562+ajimoukhuede@users.noreply.github.com>
2019-02-02 19:41:14 -0500

    Add files via upload

---
+++ OMBD.js
@@ -0,0 +1,28 @@
+var axios = require("axios");
+
+// Grab the movieName which will always be the third node argument.
+var movieName = process.argv.splice(2).join(" ");
+
+// Then run a request with axios to the OMDB API with the movie specified
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "tt3896198&apikey=f1164702"
+
+
+// This line is just to help us debug against the actual URL.
+
+axios.get(queryUrl).then(
+  function(response) {
+      console.log("|^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^|");
+
+    console.log("Release Title: " + response.data.Title);
+    console.log("Runtime: " + response.data.Runtime);
+    console.log("The Release Year was: " + response.data.Year);
+    console.log("ImdbRating: " + response.data.imdbRating);
+    console.log("Rated: " + response.data.Rated);
+    console.log("Country produced: " + response.data.Country);
+    console.log("Language of movie: " + response.data.Language);
+    console.log("Plot of the Movie: " + response.data.Plot);
+    console.log("Actors of the Movie: " + response.data.Actors);
+
+    console.log("|______________________________________|");
+  }
+);
---
+++ keys.js
@@ -0,0 +1,6 @@
+console.log('this is loaded');
+
+exports.spotify = {
+ id:  "19523b92d1314655b102dd14fb5a9bdc",
+ secret: "9f4534fe93ea4df4bfef86ae325cb86a"
+};
